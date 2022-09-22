7b830afbbcc43ebadfb207c02dbc5dd631b7a165
Stein Milder <steinmilder@mbp-van-stein.lan>
2022-09-22 15:57:07 +0200

    Update auth_screen.dart

--- lib/src/auth_screen.dart
+++ lib/src/auth_screen.dart
@@ -31,6 +31,7 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
         title: Text(widget.title),
       ),
