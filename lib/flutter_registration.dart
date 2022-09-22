5a6e195650d184c8bcb413ad21a8b465a405a85d
Stein Milder <steinmilder@mbp-van-stein.lan>
2022-09-22 09:47:31 +0200

    fix: export models to use externally

--- lib/flutter_registration.dart
+++ lib/flutter_registration.dart
@@ -1,5 +1,7 @@
 library flutter_registration;
 
 export 'src/model/auth_exception.dart';
+export 'src/model/auth_field.dart';
+export 'src/model/auth_step.dart';
 export 'src/registration_screen.dart';
 export 'src/service/registration_repository.dart';
