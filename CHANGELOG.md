16661859207d56a955c7177eef9bb0cfa3833d91
Bugfix Jacques <jacques@iconica.nl>
2023-02-16 15:02:03 +0100

    feat: Added second textfield too password and added more styling options

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,11 +4,16 @@ SPDX-FileCopyrightText: 2022 Iconica
 SPDX-License-Identifier: GPL-3.0-or-later
 -->
 
-## 0.0.1
+## 0.2.0
 
-* Initial version 
+- Added the abilty to add labels
+- The default password step now includes two textfields
 
 ## 0.0.2
 
-* Firebase integration
-* Registration capabilities
+- Firebase integration
+- Registration capabilities
+
+## 0.0.1
+
+- Initial version
--- lib/src/config/registration_translations.dart
+++ lib/src/config/registration_translations.dart
@@ -10,12 +10,17 @@ class RegistrationTranslations {
     this.nextStepBtn = 'Volgende stap',
     this.closeBtn = 'Sluiten',
     this.defaultEmailTitle = 'Wat is je e-mailadres?',
+    this.defaultEmailLabel = '',
     this.defaultEmailHint = 'iemand@voorbeeld.nl',
     this.defaultEmailEmpty = 'Geef uw e-mailadres op',
     this.defaultEmailValidatorMessage = 'Geef een geldig e-mailadres op',
-    this.defaultPasswordTitle = 'Kies een wachtwoord',
-    this.defaultPasswordHint = '',
-    this.defaultPasswordValidatorMessage = 'Geef een wachtwoord op',
+    this.defaultPassword1Title = 'Kies een wachtwoord',
+    this.defaultPassword1Label = '',
+    this.defaultPassword1Hint = '',
+    this.defaultPassword1ValidatorMessage = 'Geef een wachtwoord op',
+    this.defaultPassword2Label = '',
+    this.defaultPassword2Hint = '',
+    this.defaultPassword2ValidatorMessage = 'Wachtwoorden moeten gelijk zijn',
   });
 
   final String title;
@@ -24,10 +29,15 @@ class RegistrationTranslations {
   final String nextStepBtn;
   final String closeBtn;
   final String defaultEmailTitle;
+  final String defaultEmailLabel;
   final String defaultEmailHint;
   final String defaultEmailEmpty;
   final String defaultEmailValidatorMessage;
-  final String defaultPasswordTitle;
-  final String defaultPasswordHint;
-  final String defaultPasswordValidatorMessage;
+  final String defaultPassword1Title;
+  final String defaultPassword1Label;
+  final String defaultPassword1Hint;
+  final String defaultPassword1ValidatorMessage;
+  final String defaultPassword2Label;
+  final String defaultPassword2Hint;
+  final String defaultPassword2ValidatorMessage;
 }
